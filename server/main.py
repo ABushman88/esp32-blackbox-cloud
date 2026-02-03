2166f10592f2af939181cccc35bbc4220ec750f5
Dominick Bush <bush5418@gmail.com>
2026-02-02 18:12:34 -0600

    Add FastAPI server with telemetry endpoint

--- server/main.py
+++ server/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import time
 
+app = FastAPI()
+
+class Telemetry(BaseModel):
+  device_id: str
+  temperature: float
+  humidity: float
+
+@app.get("/")
+def root():
+  return {"status" : "Blackbox online"}
+
+@app.post("/telemetry")
+def receive_data(data: Telemetry):
+  print("DATA:", data)
+  return {"status" : "logged", "timestamp": time.time()}
